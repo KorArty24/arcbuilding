345f5447a4225208f81d019d84f0d25e2dffb82b
KorArty24 <48129496+KorArty24@users.noreply.github.com>
2023-01-02 04:02:37 +0300

    UpperLowerCord todo Web.

---
+++ WindowsFormsApp1/Arc/SectionedBeam.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tekla.Structures.Model;
+
+namespace WindowsFormsApp1.Arc
+{
+    public static class SectionedBeam
+    {
+        public static Beam CreateUpperChordBeam() 
+        {
+            Beam upperchord = new Beam(Beam.BeamTypeEnum.BEAM);
+            upperchord.Material.MaterialString = "S255";
+            upperchord.Profile.ProfileString = "HEA300";
+            upperchord.Class = "4";
+            return upperchord;
+        }
+    }
+}
--- WindowsFormsApp1/Arc/TArc.cs
+++ WindowsFormsApp1/Arc/TArc.cs
@@ -9,48 +9,77 @@ using System.Drawing;
 using Tekla.Structures.Geometry3d;
 using Point = Tekla.Structures.Geometry3d.Point;
 using WindowsFormsApp1.Arc;
+using Tekla.Structures.Model;
 
 namespace WindowsFormsApp1.TArc
 {
     internal class TArc
     {
-        public void BuildArc()
+        public void BuildArc(ArcDTO dto)
         {
-            ArcDTO dto = new ArcDTO();
             double height =dto.height;
             double span = dto.span;
             const int numPanels = 6; // temrprary for testing purposes
-            double radiusUp = height;
+            double radiusY = height;
+            double radiusX = dto.span/2;
             //gets the center of the arc B(a, r) where a is the center, r - radius
-            (double x, double y) center = (span * 0.5, height - radiusUp);
-            var centerpoint = new Point(span/2, center.y);
+            (double x, double y) center = (span * 0.5, height - radiusY);
+            var centerpoint = new Point(center.x, center.y);
             //Central angle. Is set to 180 deg (PI)
             const double  centralAngle = 180;
             // Calculates the sector angle (180/6 = 30)
             int stepsize = (int) centralAngle / numPanels;
-
+            int startAngle = 180; //Deg
+            var innerPoints = new List<Point>();
+            var pointsUpper = new List<Point>();
             DrawUpperArc();
+            DrawInnerArc();
+            CreateUpperChord();
+            CreateLowerChord();
 
             void DrawUpperArc()
             {
-                List<Point> pointsUpper = new List<Point>();
-                int startAngle = 180; //Deg
                 int endAngle =0; //Deg
-
-                    for (int i= startAngle; i<= endAngle; i = i - stepsize)
+                    for (double i= startAngle; i< endAngle; i = i - stepsize)
                     {
                         /*Calculates points according to x = r cos(\fi)  y = r cos(\fi) */
-                        double coordX = radiusUp * Math.Cos(Trigonometry.ToRadians(i));
-                        double coordY = radiusUp * Math.Sin(Trigonometry.ToRadians(i));
-                        Point point = new Point(coordX, coordY);
-                        pointsUpper.Add(point);
+                        double coordX = radiusX * Math.Cos(Trigonometry.ToRadians(i));
+                        double coordY = radiusY * Math.Sin(Trigonometry.ToRadians(i));
+                        pointsUpper.Add(new Point(coordX, coordY));
                     }
             }
-            void DrawInnerArc (int nChordes, int startangle, int endAngle) 
+            void DrawInnerArc () 
             {
-                //int centralAngleInner = centralAngle +
-                int startAngleInner = startangle - stepsize/2;
+                double startAngleInner = startAngle - stepsize/2;
+                double endAngle = stepsize / 2;
+                for (double i = startAngleInner; i < endAngle; i = i - stepsize) 
+                {
+                    double coordX = radiusX * Math.Cos(Trigonometry.ToRadians(i));
+                    double coordY = radiusY * Math.Sin(Trigonometry.ToRadians(i));
+                    innerPoints.Add(new Point(coordX, coordY));
+                }
+            }
 
+            void CreateUpperChord()
+            {
+                var upperChord = SectionedBeam.CreateUpperChordBeam();
+                for (int i = 0; i < pointsUpper.Count-1; i++) 
+                {
+                    upperChord.StartPoint = pointsUpper[i];
+                    upperChord.EndPoint = pointsUpper[i + 1];
+                    upperChord.Insert();
+                }
+            }
+
+            void CreateLowerChord() 
+            {
+                var upperChord = SectionedBeam.CreateUpperChordBeam();
+                for (int i = 0; i < innerPoints.Count-1; i++) 
+                {
+                    upperChord.StartPoint = innerPoints[i];
+                    upperChord.EndPoint = innerPoints[i + 1];
+                    upperChord.Insert();
+                }
             }
         }
     }
